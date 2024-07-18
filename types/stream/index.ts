6095a3d943cb414ce25efaf24ae77addad815999
mguleryuz <mguleryuz3@gmail.com>
2024-07-18 21:36:28 +0300

    chore: lts

--- types/stream/index.ts
+++ types/stream/index.ts
@@ -1,10 +1,10 @@
-import { EventType } from '../api'
-import type { IssuanceTokenStramEvent } from './issuanceToken'
-export * from './issuanceToken'
+import type { EventType } from "../api";
+import type { IssuanceTokenStramEvent } from "./issuanceToken";
+export * from "./issuanceToken";
 
 export type SSEMessage<T extends EventType = EventType> = {
-  data: IssuanceTokenStramEvent<T>
-  event?: T
-  id?: string
-  retry?: number
-}
+  data: IssuanceTokenStramEvent<T>;
+  event?: T;
+  id?: string;
+  retry?: number;
+};
--- types/stream/issuanceToken.ts
+++ types/stream/issuanceToken.ts
@@ -1,20 +1,20 @@
-import { EEventType, EventType } from '../api'
+import { EEventType, type EventType } from "../api";
 import type {
   IssuanceTokenResponse,
   PreInitilizeIssuanceTokenResponse,
-} from '../issuanceToken'
+} from "../issuanceToken";
 
 export type IssuanceTokenStramEventBase =
   | {
-      type: EEventType.ISSUANCE_TOKEN_UPDATE
-      data: IssuanceTokenResponse
+      type: EEventType.ISSUANCE_TOKEN_UPDATE;
+      data: IssuanceTokenResponse;
     }
   | {
-      type: EEventType.ISSUANCE_TOKEN_INSERT
-      data: PreInitilizeIssuanceTokenResponse
-    }
+      type: EEventType.ISSUANCE_TOKEN_INSERT;
+      data: PreInitilizeIssuanceTokenResponse;
+    };
 
 export type IssuanceTokenStramEvent<T extends EventType = EventType> = Extract<
   IssuanceTokenStramEventBase,
   { type: T }
->['data']
+>["data"];
